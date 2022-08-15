53764f6d9cb19e8abc4de6716a6ed70efe72a03d
olivia pasion <oap9820@gmail.com>
2022-08-15 15:38:59 -0700

    changed create function to signUp, returns token for new Users to immediately login

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -9,14 +9,9 @@ describe('backend-express-template routes', () => {
   });
   it('#POST /api/v1/users should create new user', async () => {
     const res = await request(app).post('/api/v1/users').send({ userName: 'test_1', email: 'abc@123', password: 'abc123' });
-    console.log(res.body);
     expect(res.status).toEqual(200);
-    expect(res.body).toEqual({
-      id: expect.any(String),
-      userName: expect.any(String),
-      email: expect.any(String),
-    });
   });
+  
   afterAll(async () => {
     await setup(pool);
     pool.end();
