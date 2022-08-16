5b86f3c9fe5cd615cb040d2e0805da895a68a19c
olivia pasion <oap9820@gmail.com>
2022-08-16 16:20:14 -0700

    #GET /restaurants passes

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -1,14 +1,21 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET /restaurants shows a list of restaurants', async () => {
+    const res = await request(app).get('/api/v1/restaurants');
+    console.log(res.body.length);
+    console.log(res.body[0]);
+    expect(res.body.length).toEqual(3);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
   });
   afterAll(() => {
     pool.end();
